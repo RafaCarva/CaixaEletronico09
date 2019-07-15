8181dcd5885025bb62af985c95269f15c2d6ea59
RafaCarva <rafael.faria.carvalho@gmail.com>
2019-07-14 23:12:02 -0300

    ajuste de namespace

--- CaixaEletronico09/CaixaEletronico09/CaixaEletronico.cs
+++ CaixaEletronico09/CaixaEletronico09/CaixaEletronico.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using CaixaEletronico09.Entities;
 
-namespace CaixaEletronico09.Entities
+namespace CaixaEletronico09
 {
 
     /// <summary>
